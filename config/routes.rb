686ff19b9c4ff4abb4cca39ff47d6e214e6d7f01
Rodrigo33024 <Rodrigo33024@gmail.com>
2016-01-29 17:10:01 -0500

    Adds amswers form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   post '/questions' => 'home#temp'
 
+  post '/answers' => 'home#temp'
+
   get '/questions/:id' => 'home#question'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
